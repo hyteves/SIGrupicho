23bc29ae937fded797e4b86432bc60f5a6d2d55e
Hyandra Teves <114451502+hyteves@users.noreply.github.com>
2025-02-25 21:31:39 +0000

    Exercicio3_Final

--- ubs_dashboard.py
+++ ubs_dashboard.py
@@ -26,13 +26,21 @@ if estados:
     df_filtrado = df[df['Nome_UF'].isin(estados)]
     st.write(df_filtrado)
 
-# Criar um histograma que exiba a quantidade de UBS por município
-df_freq = df['Nome_UF'].value_counts().reset_index()
-df_freq.columns = ['Estado', 'Frequência']
-
 # Histograma da Quantidade de UBS por Município
+# Contar a frequência de UBS por município
+df_mun_freq = df['Nome_Município'].value_counts().reset_index()
+df_mun_freq.columns = ['Município', 'Frequência']
 
-import plotly.express as px
-df = px.data.tips()
-fig = px.histogram(df, x="total_bill")
-fig.show()
+# Controle deslizante para o número mínimo de UBS
+num_min_ubs = st.slider("Selecione o número mínimo de UBS", min_value=1, max_value=int(df_mun_freq['Frequência'].max()), value=1)
+
+# Filtrar municípios com pelo menos o número mínimo de UBS
+df_mun_filtrado = df_mun_freq[df_mun_freq['Frequência'] >= num_min_ubs]
+
+# Criar histograma
+grafico_histograma = px.histogram(df_mun_filtrado, x='Município', y='Frequência',
+                                  title='Quantidade de UBS por Município',
+                                  labels={'Município': 'Município', 'Frequência': 'Número de UBS'},
+                                  text_auto=True)
+
+st.plotly_chart(grafico_histograma)
