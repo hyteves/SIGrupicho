fb2000fef7f3edab9cd638c708fb44d295d8c37b
Gabriel Tofanello <126465879+gstofanello@users.noreply.github.com>
2025-02-25 01:32:08 +0000

    first try map

--- ubs_dashboard.py
+++ ubs_dashboard.py
@@ -21,7 +21,22 @@ grafico = px.bar(df_freq, x='Estado', y='Frequência',
 st.plotly_chart(grafico)
 
 # Filtro para estados específicos
-estados = st.multiselect("Selecione os estados", df_freq['Estado'].unique())
+estados = st.multiselect("Selecione os estados para tabela", df_freq['Estado'].unique(), key="multiselect_tabela")
 if estados:
     df_filtrado = df[df['Nome_UF'].isin(estados)]
     st.write(df_filtrado)
+
+# Filtro para estados específicos (para o mapa)
+estados_mapa = st.multiselect("Selecione os estados para mapa", df_freq['Estado'].unique(), key="multiselect_mapa")
+if estados_mapa:
+    df_filtrado_mapa = df[df['Nome_UF'].isin(estados_mapa)]
+
+    # Mapa de dispersão para localização das UBS
+    mapa = px.scatter_geo(df_filtrado_mapa, lat='LATITUDE', lon='LONGITUDE', hover_name='Nome_UF', 
+                      title='Localização das UBS Selecionadas',
+                      scope='south america', 
+                      labels={'Nome_UF': 'Estado'})
+    mapa.update_geos(showcoastlines=True, coastlinecolor="Black", showland=True, landcolor="white")
+    st.plotly_chart(mapa)
+else:
+    st.write("Selecione um ou mais estados para visualizar as UBS no mapa.")
